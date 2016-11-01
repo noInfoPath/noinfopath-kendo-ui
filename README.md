87b56a203652d13b2878f06f9c6311bb42a9693c
Jeff <jeff@imginconline.com>
2016-11-01 17:15:23 -0400

    Moved some methods from noKendoRowTemplates to noKendoHelpers. Update documentation for noKendoHelpers.

--- README.md
+++ README.md
@@ -224,3 +224,63 @@ value for the child grid.
   getting and setting data.
 
 
+## NoKendoHelpersService
+
+> @service noKendoHelpers
+
+This services provide various helper functions that provide access
+details about the a given row of data in a grid, as well as, access to
+grid's currently selected row.
+
+> NOTE: A future enhancements will be that it allows for multi-row selection,
+> and cell slections.
+
+### @method getConfigMethod
+
+This is a specialty function that helps NoInfoPath wrapped widgets
+determine where to read thier configuration data from.
+
+> NOTE: This function may be either deprecated to relocated to
+> NoInfoPath Helpers module in the future.
+
+### @method resolveConfigType
+
+This is a specialty function that is typically used in conjection with
+`getConfigMethod`. It helps NoInfoPath wrapped widgets
+resolve what type of configuration data a directive is using..
+
+> NOTE: This function may be either deprecated to relocated to
+> NoInfoPath Helpers module in the future.
+
+### @method getGridRow
+
+This method, given a `jQuery` element, returns the closest parent
+that matches the `tr[data-uid]` selector, as a jQuery element.
+
+This method is especially useful when used in conjection with
+NoInfoPath's noActionQueue service to resolve action parameters
+for Kendo Grid methods that require a row element as one of its
+parameters. It is usually expected that the action be attached to
+button that is child of a given row.
+
+
+### @method getGridRowUID
+
+This method, given a `jQuery` element, returns the data-uid of the
+supplied element's parent row that matches the `tr[data-uid]` selector.
+
+This method is especially useful when used in conjection with
+NoInfoPath's noActionQueue service to resolve action parameters
+for Kendo Grid methods that require a row data-uid as one of its
+parameters. It is usually expected that the action be attached to
+button that is child of a given row.
+
+
+### @method getSelectedGridRow
+
+### @method getSelectedGridRowData
+
+### @method currentGridRowData
+
+### @method currentGridRow
+
--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,27 @@
 //helpers.js
 (function(angular) {
-
+	/**
+	*	## NoKendoHelpersService
+	*
+	*	> @service noKendoHelpers
+	*
+	*	This services provide various helper functions that provide access
+	*	details about the a given row of data in a grid, as well as, access to
+	*	grid's currently selected row.
+	*
+	*	> NOTE: A future enhancements will be that it allows for multi-row selection,
+	*	> and cell slections.
+	*/
 	function NoKendoHelpersService($injector, $compile, $state) {
+		/*
+		*	### @method getConfigMethod
+		*
+		*	This is a specialty function that helps NoInfoPath wrapped widgets
+		*	determine where to read thier configuration data from.
+		*
+		*	> NOTE: This function may be either deprecated to relocated to
+		*	> NoInfoPath Helpers module in the future.
+		*/
 		this.getConfigMethod = function(type) {
 			var cfgFn = {
 					"noConfig": function($injector, $compile, $state, attrs, editor) {
@@ -52,6 +72,16 @@
 			return (method || cfgFn.noForm).bind(null, $injector, $compile, $state);
 		};
 
+		/**
+		*	### @method resolveConfigType
+		*
+		*	This is a specialty function that is typically used in conjection with
+		*	`getConfigMethod`. It helps NoInfoPath wrapped widgets
+		*	resolve what type of configuration data a directive is using..
+		*
+		*	> NOTE: This function may be either deprecated to relocated to
+		*	> NoInfoPath Helpers module in the future.
+		*/
 		this.resolveConfigType = function(attrs) {
 			var configurationType;
 
@@ -68,12 +98,38 @@
 			return configurationType;
 		};
 
+		/**
+		*	### @method getGridRow
+		*
+		*	This method, given a `jQuery` element, returns the closest parent
+		*	that matches the `tr[data-uid]` selector, as a jQuery element.
+		*
+		*	This method is especially useful when used in conjection with
+		*	NoInfoPath's noActionQueue service to resolve action parameters
+		*	for Kendo Grid methods that require a row element as one of its
+		*	parameters. It is usually expected that the action be attached to
+		*	button that is child of a given row.
+		*
+		*/
 		function _getGridRow(el) {
 			var tr = el.closest("tr[data-uid");
 			return $(tr);
 		}
 		this.getGridRow = _getGridRow;
 
+		/**
+		*	### @method getGridRowUID
+		*
+		*	This method, given a `jQuery` element, returns the data-uid of the
+		*	supplied element's parent row that matches the `tr[data-uid]` selector.
+		*
+		*	This method is especially useful when used in conjection with
+		*	NoInfoPath's noActionQueue service to resolve action parameters
+		*	for Kendo Grid methods that require a row data-uid as one of its
+		*	parameters. It is usually expected that the action be attached to
+		*	button that is child of a given row.
+		*
+		*/
 		function _getGridRowUID(el) {
 			var tr = _getGridRow(el),
 				uid = tr.attr("data-uid");
@@ -82,11 +138,17 @@
 		}
 		this.getGridRowUID = _getGridRowUID;
 
+		/**
+		*	### @method getSelectedGridRow
+		*/
 		function _getSelectedGridRow(grid) {
 			return grid.select();
 		}
 		this.getSelectedGridRow = _getSelectedGridRow;
 
+		/**
+		*	### @method getSelectedGridRowData
+		*/
 		function _getSelectedGridRowData(grid) {
 			var tr = _getSelectedGridRow(grid),
 				data = grid.dataItem(tr);
@@ -94,6 +156,27 @@
 			return data;
 		}
 		this.getSelectedGridRowData = _getSelectedGridRowData;
+
+
+		/**
+		*	### @method currentGridRowData
+		*/
+		this.currentGridRowData = function(scope, el) {
+			var tr = _getGridRow(el),
+				grid = scope.noGrid,
+				data = grid.dataItem(tr);
+
+
+			return data;
+		};
+
+		/**
+		*	### @method currentGridRow
+		*/
+		this.currentGridRow = function(scope, el) {
+			var tr = _getGridRow(el);
+			return tr;
+		};
 
 	}
 
