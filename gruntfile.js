87dd1fecbbd9a27b7d8106966775d2adadb5824a
Jeffrey A. Gochin <jeff@imginconline.com>
2017-04-04 15:14:24 -0400

    Moved noKendoHelpers service to noinfopath-helpers

--- gruntfile.js
+++ gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
                     'src/datepicker.js',
                     'src/multiselect.js',
                     'src/autocomplete.js',
-                    'src/helpers.js',
+
                     'src/lookup.js'
                 ],
                 dest: 'dist/noinfopath-kendo-ui.js'
