103d0d1426e8eb9f4359ccf9b9c9d8a6000d3e31
Jeff <jeff@imginconline.com>
2017-05-08 11:36:35 -0400

    Bumped version to 2.0.24

--- src/global.js
+++ src/global.js
@@ -6,7 +6,7 @@
  *
  *	___
  *
- *	[NoInfoPath Kendo UI (noinfopath-kendo-ui)](home) *@version 2.0.23*
+ *	[NoInfoPath Kendo UI (noinfopath-kendo-ui)](home) *@version 2.0.24*
  *
  *	Copyright (c) 2017 The NoInfoPath Group, LLC.
  *
